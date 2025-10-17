5f5827faa7e7bfe05708bfc71eca618ce7c3537a
LudmiAbril <ludmila.pereyra543@gmail.com>
2025-10-17 00:01:29 -0300

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
+import WelcomePage from './pages/WelcomePage'
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<WelcomePage />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+export default App
---
+++ src/pages/WelcomePage.tsx
@@ -0,0 +1,7 @@
+const WelcomePage = () => {
+    return (
+        <div>Welcome</div>
+    )
+}
+
+export default WelcomePage
